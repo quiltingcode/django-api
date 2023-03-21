2f939dde1a814ea07c44b687abebd120e39e4fb7
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-21 16:16:10 +0000

    Add put method to profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.http import Http404
@@ -13,6 +14,8 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
@@ -24,3 +27,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
