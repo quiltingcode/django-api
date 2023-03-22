dc06097bdee08ef16a799be5d8cc680f9620babc
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-22 11:46:59 +0000

    Add image filter to model and parameters to serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,13 +8,29 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
-        model = Profile
+        model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
-            'content', 'image', 'is_owner', 'profile_id', 'profile_image'
+            'content', 'image', 'is_owner', 'profile_id', 'profile_image',
+            'image_filter'
         ]
