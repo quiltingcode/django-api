d04b58466715c31f7aff4cf9575bb1d347e37d97
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-23 14:17:33 +0000

    Add followers views and urls

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import isOwnerOrReadOnly
+from .models import Followers
+from .serializers import FollowersSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    serializer_class = FollowersSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Followers.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [isOwnerOrReadOnly]
+    serializer_class = FollowersSerializer
+    queryset = Followers.objects.all()
