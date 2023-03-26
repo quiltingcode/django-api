47ec8945f2652ae5207ebfed57b3bc827e7a0cd4
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-26 13:47:37 +0000

    Add sortable fields to profilelist

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import isOwnerOrReadOnly
@@ -6,7 +7,21 @@ from drf_api.permissions import isOwnerOrReadOnly
 
 class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__create_at',
+        'owner__followed__created_at'
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
