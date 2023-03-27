6dadb36af99d8f5022ae4f25d24494412354a0b4
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-27 14:50:11 +0000

    Change host settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'django-rest-kelz.herokuapp.com']
+ALLOWED_HOSTS = ['django-rest-kelz.herokuapp.com', 'localhost']
 
 
 # Application definition
